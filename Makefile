25c22d5570b328a840d0388a2c73965edd62c4c9
Rob Stein <rob@Robs-MacBook-Pro.local>
2018-06-19 02:17:29 -0700

    ALSA rawmidi in sample

---
+++ Makefile
@@ -0,0 +1,2 @@
+midi_in:
+	gcc midi_in.c -lasound -o midi_in
---
+++ midi_in.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <alsa/asoundlib.h>
+#include <alsa/rawmidi.h>
+#include <signal.h>
+
+int stop=0;
+void sighandler(int dum) {
+	stop = 1;
+}
+
+int main() {
+	int err;
+	snd_rawmidi_t *handle_in = 0;
+	char *node_in = NULL;
+	
+	err = snd_rawmidi_open(&handle_in, NULL, "hw:1,0,0", 0);
+	if (err) {
+		fprintf(stderr,"snd_rawmidi_open %s failed: %d\n",err);
+	}
+		
+	signal(SIGINT, sighandler);
+	
+	if (handle_in) {
+		fprintf(stderr,"Read midi in\n");
+		fprintf(stderr,"Press ctrl-c to stop\n");
+	}
+
+	if (handle_in) {
+		unsigned char ch;
+		while (!stop) {
+			snd_rawmidi_read(handle_in,&ch,1);
+			fprintf(stderr,"read %02x\n",ch);
+		}
+	}
+	
+	if (handle_in) {
+		snd_rawmidi_drain(handle_in); 
+		snd_rawmidi_close(handle_in);   
+	}
+
+	return 0;
+}
