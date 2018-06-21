3eaa05d6fdcd90a8be24ac28cff524d682d271d4
Rob Stein <rob@Robs-MacBook-Pro.local>
2018-06-21 02:31:04 -0700

    Make it LEDs look better

--- couscous/couscous.go
+++ couscous/couscous.go
@@ -6,7 +6,6 @@ import (
 	"github.com/robstein/couscous/midi"
 	"os"
 	"os/signal"
-	"rand"
 	"syscall"
 	"unsafe"
 )
@@ -24,11 +23,15 @@ func main() {
 	midiMessages := midi.Parse(c)
 
 	defer ws2811.Fini()
-	err := ws2811.Init(18, 101, 255)
+	err := ws2811.Init(18, KeyCount, 255)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "ws2811.Init failed: %d\n", err)
 	} else {
 		writeToLEDs(midiMessages)
+	}
+
+	for {
+
 	}
 }
 
@@ -62,17 +65,32 @@ func inputStream(midiHandle string) <-chan byte {
 	return out
 }
 
-const MidiKeyboardOffset = 21
+const KeyCount = 88
+const MidiKeyboardOffset = 28
 
-func writeToLEDs(in <-chan Message) {
+func writeToLEDs(in <-chan midi.Message) {
 	go func() {
+		pedal_off := true
 		for m := range in {
-			fmt.Printf("Writing %s %d %d\n", message.Status, message.Data1, message.Data2)
+			fmt.Printf("%s %d %d\n", m.Status, m.Data1, m.Data2)
 
-			if message.Status == NoteOn {
-				ws2811.SetLed(message.Data1-MidiKeyboardOffset, rand.Uint32())
-			} else {
-				ws2811.SetLed(message.Data1-MidiKeyboardOffset, 0)
+			if m.Status == midi.ControlChange && m.Data1 == 64 && m.Data2 == 127 {
+				pedal_off = false
+			}
+			if m.Status == midi.ControlChange && m.Data1 == 64 && m.Data2 == 0 {
+				pedal_off = true
+			}
+
+			led := m.Data1 - MidiKeyboardOffset
+			if m.Status == midi.NoteOn {
+				color := getColor(m.Data1, m.Data2)
+				ws2811.SetLed(led, color)
+			} else if m.Status == midi.NoteOff && pedal_off {
+				ws2811.SetLed(led, 0)
+			} else if pedal_off {
+				for i := 0; i < KeyCount; i++ {
+					ws2811.SetLed(i, 0)
+				}
 			}
 
 			err := ws2811.Render()
@@ -82,4 +100,35 @@ func writeToLEDs(in <-chan Message) {
 			}
 		}
 	}()
+}
+
+func getColor(i int, v int) uint32 {
+	out := 0xFFFFF
+	switch i % 12 {
+	case 0:
+		out = 0x9400D3
+	case 1:
+		out = 0x4B0082
+	case 2:
+		out = 0x26007D
+	case 3:
+		out = 0x0000FF
+	case 4:
+		out = 0x008080
+	case 5:
+		out = 0x00FF00
+	case 6:
+		out = 0x80FF00
+	case 7:
+		out = 0xFFFF00
+	case 8:
+		out = 0xFFC000
+	case 9:
+		out = 0xFF7F00
+	case 10:
+		out = 0xFF4000
+	case 11:
+		out = 0xFF0000
+	}
+	return uint32(out)
 }
